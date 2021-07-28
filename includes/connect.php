b3bc22af3997c9f2a52fbb0ea94a8f8d07e903a6
Muhammad Syahmi <syahmijalil12@gmail.com>
2021-07-28 13:12:30 +0800

    Push

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
 	
 	// Create connection
-	$conn =mysqli_connect("localhost","muhammad_user1","H89EPXkT^^m4","muhammad_db");
+	$conn =mysqli_connect("localhost","syahmi_user1","XgZb8]?{Xn3s","syahmi_muhammad_db");
 
 	// Check connection
 	if (mysqli_connect_errno())
